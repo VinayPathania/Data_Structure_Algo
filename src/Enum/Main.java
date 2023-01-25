29212594268dd3dc666a6e98b26e125d10c87a49
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2023-01-25 18:20:18 +0530

    Enum added

---
+++ src/Enum/Main.java
@@ -0,0 +1,11 @@
+package Enum;
+
+public class Main {
+    public static void main(String[] args) {
+        for(Week_days days: Week_days.values()){
+            System.out.println(days);
+        }
+
+    }
+
+}
---
+++ src/Enum/Week_days.java
@@ -0,0 +1,16 @@
+package Enum;
+
+public enum Week_days {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+
+    // Enums are constants
+    // They are only public, static, final
+    // since its final you can create child enums
+
+
+}
