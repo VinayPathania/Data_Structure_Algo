a06d6533ca341e5a4c19a6eeae719e7b0149aabe
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2023-01-26 14:14:45 +0530

    Singly LinkedList 2 method added 1. InsertFirst 2. InsertLast

--- src/LinkedList/RunnerClass.java
+++ src/LinkedList/RunnerClass.java
@@ -2,6 +2,15 @@ package LinkedList;
 
 public class RunnerClass {
     public static void main(String[] args) {
-        
+        SinglyLL list = new SinglyLL();
+        list.insertFirst(2);
+        list.insertFirst(4);
+        list.insertFirst(9);
+        list.insertFirst(7);
+
+        list.insertLast(99);
+
+        list.insertFirst(200);
+        list.display();
     }
 }
