e1f3616b0dec2852868d8bae7ddf08f1e3f3383d
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2023-02-02 21:27:16 +0530

    Binary Search two example questions added

---
+++ src/Binary_Search/Example_Problems/Floor_of_number.java
@@ -0,0 +1,23 @@
+package Binary_Search.Example_Problems;
+
+public class Floor_of_number {
+    static int floor(int[] arr, int target){
+        int start=0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(target < arr[mid]){
+                end = mid-1;
+            }else if(target>arr[mid]){
+                start = mid+1;
+            }else return arr[mid];
+        }
+        return arr[end];
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,9,12,17,19,22};
+        int target =15;
+        System.out.println(floor(arr,target));
+    }
+}
